5037acf786ae619b4746ab239e482f8f08e58c1a
deadblue <root@dead.blue>
2020-07-21 08:13:21 +0800

    Check command name before run.

--- internal/pkg/engine/task/command.go
+++ internal/pkg/engine/task/command.go
@@ -3,9 +3,12 @@ package task
 import (
 	"bytes"
 	"context"
+	"errors"
 	"io/ioutil"
 	"os/exec"
 )
+
+var errNoCommand = errors.New("no command to execute")
 
 type CommandTask struct {
 	baseTask
@@ -15,23 +18,27 @@ type CommandTask struct {
 }
 
 func (t *CommandTask) Run(ctx context.Context) (err error) {
-	// Make request
+	if t.name == "" {
+		return errNoCommand
+	}
+	// Make command
 	cmd := exec.CommandContext(ctx, t.name, t.args...)
 	// Input data
 	if t.input != nil {
 		cmd.Stdin = bytes.NewReader(t.input)
 	}
 	// Capture stdout when need
+	stdout := (*bytes.Buffer)(nil)
 	if t.cb != nil {
-		cmd.Stdout = &bytes.Buffer{}
+		stdout = &bytes.Buffer{}
+		cmd.Stdout = stdout
 	} else {
 		cmd.Stdout = ioutil.Discard
 	}
 	cmd.Stderr = ioutil.Discard
-	// Run command
+	// Execute command
 	if err = cmd.Run(); err == nil {
-		if t.cb != nil {
-			stdout := cmd.Stdout.(*bytes.Buffer)
+		if t.cb != nil && stdout != nil {
 			go t.cb.Send(stdout.Bytes())
 		}
 	}
