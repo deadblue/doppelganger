ea2d25c6c4fb5c4cdf29f3ff6923940303d9e58f
deadblue <root@dead.blue>
2020-07-24 12:14:46 +0800

    Add LoadConf.

--- internal/app/daemon/config.go
+++ internal/app/daemon/config.go
@@ -1,5 +1,11 @@
 package daemon
 
+import (
+	"github.com/deadblue/gostream/quietly"
+	"gopkg.in/yaml.v2"
+	"os"
+)
+
 type QueueConf struct {
 	CoreSize int `yaml:"core_size"`
 }
@@ -23,4 +29,13 @@ type Conf struct {
 	DefaultQueue *QueueConf        `yaml:"default_queue"`
 	Queues       []*NamedQueueConf `yaml:"queues"`
 	Server       *ServerConf       `yaml:"server"`
+}
+
+func LoadConf(path string, conf *Conf) (err error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return
+	}
+	defer quietly.Close(file)
+	return yaml.NewDecoder(file).Decode(conf)
 }
